279e4584e5061238f614cf46ffbae9d7cb6d86ee
Malfoy <antoine.limasset@gmail.com>
2016-11-29 17:31:01 +0100

    two step dbg

--- assemble.sh
+++ assemble.sh
@@ -8,8 +8,10 @@ echo "-x for paired read file"
 echo "-u for unpaired read file"
 echo "-k for kmer size"
 echo "-o for working folder"
-echo "-x for paired read file"
-echo "-s for kmer solidity threshols"
+echo "-s for kmer solidity threshold"
+echo "-K for large kmer size"
+echo "-S for large solidity threshold"
+echo "-t for thread number"
 }
 
 
@@ -21,8 +23,10 @@ outputFolder="folderAssembly"
 readFileName=""
 pairedReadFileName=""
 kmersize=100
-solidity=10
-pathsSolidity=3
+kmersize2=200
+solidity=5
+solidity2=1
+pathsSolidity=1
 
 while getopts "hx:u:k:o:s:" opt; do
 case $opt in
@@ -42,6 +46,12 @@ echo "use kmer solidity threshold: $OPTARG" >&2
 solidity=$OPTARG
 ;;
 
+S)
+echo "use large kmer solidity threshold: $OPTARG" >&2
+solidity2=$OPTARG
+;;
+
+
 u)echo "use unpaired read file: $OPTARG" >&2
 readFileName=$OPTARG
 ;;
@@ -55,6 +65,12 @@ k)
 echo "use k: $OPTARG" >&2
 kmersize=$OPTARG
 ;;
+
+K)
+echo "use K: $OPTARG" >&2
+kmersize2=$OPTARG
+;;
+
 
 \?)
 echo "Invalid option: -$OPTARG" >&2
@@ -111,12 +127,24 @@ fi
 
 
 
+echo "Parameters used for this assembly :
+unpaired read file used $readFileName=
+paired file used: $pairedReadFileName
+kmersize small graph: $kmersize
+kmer size large graph: $kmersize2
+solidity small graph: $solidity
+solidity large graph: $solidity2
+path solidity: $pathsSolidity
+" > ParametersUsed.txt 
+
+
 #READ CORRECTION
 
 
 
 startcorrection_time=`date +%s`
-../bloocoo//build/bin/Bloocoo -file $bloocooString  -kmer-size 31 -abundance-min 5 -out reads_corrected.fa >>log 2>>log;
+../bloocoo/build/bin/Bloocoo -file $bloocooString  -kmer-size 31 -abundance-min 5 -out reads_corrected.fa >>log 2>>log;
+#cp $bloocooString reads_corrected.fa 
 endcorrection_time=`date +%s`
 echo 1/5 reads corrected in `expr $endcorrection_time - $startcorrection_time` seconds;
 
@@ -140,9 +168,34 @@ echo 2/5 graph constructed in `expr $endgraph_time - $startgraph_time` seconds;
 
 
 startmap_time=`date +%s`
-../BGREAT2/bgreat -k $kmersize $bgreatString -g out.fa -t 8  -c -m 0 -e 1 >>log 2>>log;
+../BGREAT2/bgreat -k $kmersize $bgreatString -g out.fa -t 20  -c -m 0 -e 1 ;
 endmap_time=`date +%s`
 echo 3/5 read mapped on graph in `expr $endmap_time - $startmap_time` seconds;
+
+
+
+#DBG CONSTRUCTION2
+
+
+
+startgraph_time=`date +%s`
+../bcalm/build/bcalm -in paths -kmer-size $kmersize2 -abundance-min $solidity2 -out out2 >>log 2>>log;
+../kMILL/src/kMILL out2.unitigs.fa $((kmersize2-1)) $((kmersize2-2))>>log 2>>log ;
+mv out_out2.unitigs.fa.fa out2.fa;
+endgraph_time=`date +%s`
+echo 4/5 large graph constructed in `expr $endgraph_time - $startgraph_time` seconds;
+
+
+
+#READ MAPPING ON DBG2
+
+
+
+startmap_time=`date +%s`
+rm paths;
+../BGREAT2/bgreat -k $kmersize2 $bgreatString -g out2.fa -t 20  -c -m 0 -e 1 ;
+endmap_time=`date +%s`
+echo 4/5 read mapped on large  graph in `expr $endmap_time - $startmap_time` seconds;
 
 
 
@@ -158,7 +211,7 @@ echo "noduplicate.fa" > bank
 ../BREADY/short_read_connector.sh -b bank -q bank >> log 2>>log;
 rm *.h5;
 endclean_time=`date +%s`
-echo 4/5 paths redundancy cleaned in `expr $endclean_time - $startclean_time` seconds;
+echo 5/5 paths redundancy cleaned in `expr $endclean_time - $startclean_time` seconds;
 
 
 
@@ -169,7 +222,7 @@ echo 4/5 paths redundancy cleaned in `expr $endclean_time - $startclean_time` se
 startass_time=`date +%s`
 ../kMILL/src/kMILL short_read_connector_res.txt >>log 2>>log;
 endass_time=`date +%s`
-echo 5/5 maximal paths compacted in `expr $endass_time - $startass_time` seconds;
+echo 6/5 maximal paths compacted in `expr $endass_time - $startass_time` seconds;
 mv out_short_read_connector_res.txt.fa contigs1.fa;
 
 
@@ -184,6 +237,50 @@ echo Total execution time was `expr $end_time - $start_time` seconds;
 n50 contigs1.fa;
 
 
+
+exit 0;
+
+
+
+
+
+
+startgraph_time=`date +%s`
+../bcalm/build/bcalm -in contigs1.fa -kmer-size 200 -abundance-min 1 -out out2 >>log 2>>log;
+../kMILL/src/kMILL out2.unitigs.fa 199 198>>log 2>>log ;
+mv out_out2.unitigs.fa.fa out2.fa;
+endgraph_time=`date +%s`
+echo 6/5 graph constructed in `expr $endgraph_time - $startgraph_time` seconds;
+
+
+
+startmap_time=`date +%s`
+../BGREAT2/bgreat -k 200 $bgreatString -g out2.fa -t 20  -c -m 0 -e 1 ;
+endmap_time=`date +%s`
+
+
+
+#duplicate superReads elimination
+startclean_time=`date +%s`
+../kMILL/src/pathsCleaner paths 3   >>log 2>>log;
+#elimination of contained superreads (non maximal one)
+echo "noduplicate.fa" > bank
+../BREADY/short_read_connector.sh -b bank -q bank >> log 2>>log;
+rm *.h5;
+endclean_time=`date +%s`
+echo 4/5 paths redundancy cleaned in `expr $endclean_time - $startclean_time` seconds;
+
+
+
+#COMPACTION OF SUPERREADS
+
+
+
+startass_time=`date +%s`
+../kMILL/src/kMILL short_read_connector_res.txt >>log 2>>log;
+
+mv out_short_read_connector_res.txt.fa contigs2.fa
+n50 contigs2.fa;
 
 
 #~ rm noduplicate.fa notAligned.fa paths reads_corrected.fasta reads_corrected.unitigs.fa reads.fasta short_read_connector_res.txt >>log 2>>log;
