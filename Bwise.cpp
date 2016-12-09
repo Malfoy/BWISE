1caa1cf13311673d257cc3a873e119a7b4240f01
Malfoy <antoine.limasset@gmail.com>
2016-12-09 15:40:24 +0100

    debug script, should work

--- Bwise.cpp
+++ Bwise.cpp
@@ -14,10 +14,8 @@
 using namespace std;
 
 
-
-int main(int argc, char *argv[]) {
-	if(argc<2){
-		cout<<"BWISE"<<endl
+void help(){
+	cout<<"BWISE"<<endl
 		<<"-x for paired read file"<<endl
 		<<"-u for unpaired read file"<<endl
 		<<"-o for working folder"<<endl
@@ -25,20 +23,28 @@ int main(int argc, char *argv[]) {
 		<<"-k for largest kmer size"<<endl
 		<<"-p for superReads cleaning threshold"<<endl
 		;
+}
+
+
+int main(int argc, char *argv[]) {
+	if(argc<2){
+
 	}
 	string pairedFile(""),unPairedFile(""),workingFolder(""),prefixCommand(""),folderStr(STR(folder));
 	uint kMax(220),solidity(2),superReadsCleaning(3);
 	if(folderStr!=""){
-		prefixCommand="./"+folderStr+"/";
+		prefixCommand=folderStr+"/";
 	}
+	//TODO working folder
+	//TODO cleaning of working folder
 	char c;
 	while ((c = getopt (argc, argv, "u:x:o:s:k:p:")) != -1){
 	switch(c){
 		case 'u':
-			pairedFile=optarg;
+			unPairedFile=optarg;
 			break;
 		case 'x':
-			unPairedFile=optarg;
+			pairedFile=optarg;
 			break;
 		case 'o':
 			workingFolder=(optarg);
@@ -54,39 +60,48 @@ int main(int argc, char *argv[]) {
 			break;
 		}
 	}
-
+	//~ cout<<folderStr<<endl;
+	//~ cin.get();
+	if(pairedFile==""){
+		help();
+		exit(0);
+	}
+	//TODO parameter reads correction number, unpaired file
 	cout<<"Reads Correction"<<endl;
-	c=system((prefixCommand+"Bloocoo -file "+pairedFile+"-kmer-size 31 -abundance-min 5 -out reads_corrected1.fa >>log 2>>log").c_str());
-	c=system((prefixCommand+"Bloocoo -file reads_corrected1.fa  -kmer-size 63 -abundance-min 5 -out reads_corrected2.fa >>log 2>>log").c_str());
-	c=system((prefixCommand+"Bloocoo -file reads_corrected2.fa  -kmer-size 127 -abundance-min 5 -out reads_corrected.fa >>log 2>>log").c_str());
+	c=system((prefixCommand+"Bloocoo -file "+pairedFile+" -kmer-size 31 -abundance-min 5 -out reads_corrected1.fa >>log 2>>log").c_str());
+	c=system((prefixCommand+"Bloocoo -file reads_corrected1.fa  -kmer-size 63 -abundance-min 5 -out reads_corrected.fa >>log 2>>log").c_str());
+	//~ c=system((prefixCommand+"Bloocoo -file reads_corrected2.fa  -kmer-size 127 -abundance-min 5 -out reads_corrected.fa >>log 2>>log").c_str());
 	cout<<"Reads Correction ended"<<endl;
 
-	vector<string> kmerList{"50","100","150","200","220"};
+	//TODO better kmerlist
+	vector<string> kmerList{"50","100","150","200",to_string(kMax)};
 	string fileBcalm(pairedFile),kmerSize;
 
 	for(uint i(0);i<kmerList.size();++i){
 		kmerSize=kmerList[i];
-		cout<<"Graph construction"<<endl;
-		c=system((prefixCommand+"bcalm -in "+fileBcalm+" -kmer-size "+kmerSize+" -abundance-min $solidity -out out >>log 2>>log").c_str());
+		cout<<"Graph construction "+to_string(i)<<endl;
+		c=system((prefixCommand+"bcalm -in "+fileBcalm+" -kmer-size "+kmerSize+" -abundance-min "+to_string(solidity)+" -out out >>log 2>>log").c_str());
 		c=system((prefixCommand+"kMILL out.unitigs.fa $(("+kmerSize+"-1)) $(("+kmerSize+"-2))>>log 2>>log").c_str());
-		c=system((prefixCommand+"mv out_out.unitigs.fa.fa out"+to_string(i)+".fa").c_str());
-		cout<<"Graph construction ended"<<endl;
+		c=system(("mv out_out.unitigs.fa.fa out"+to_string(i)+".fa").c_str());
+		cout<<"Graph construction "+to_string(i)+"  ended"<<endl;
 
-		cout<<"Read mapping on the graph"<<endl;
+		cout<<"Read mapping on the graph "+to_string(i)<<endl;
 		c=system((prefixCommand+"bgreat -k "+kmerSize+" "+pairedFile+" -g out.fa -t 20  -c -m 0 -e 1 >>log 2>>log").c_str());
-		cout<<"Read mapping on the graphn ended"<<endl;
+		cout<<"Read mapping on the graph "+to_string(i)+" ended "<<endl;
 		fileBcalm="paths";
 	}
 
 	cout<<"SuperReads Cleaning"<<endl;
-	c=system((prefixCommand+"pathsCleaner paths $pathsSolidity >>log 2>>log").c_str());
-	c=system(("cat out5.fa >> noduplicate.fa"));
-	c=system((prefixCommand+"dsk -file noduplicate.fa -kmer-size 220 -abundance-min 1 -out out_dsk").c_str());
-	c=system((prefixCommand+"bready -graph out_dsk -bank noduplicate.fa -query noduplicate.fa -out maximalSuperReads.fa -kmer_threshold 1 -fingerprint_size 60 -core 0 -gamma 10").c_str());
+	c=system((prefixCommand+"pathsCleaner paths "+to_string(superReadsCleaning)+" >>log 2>>log").c_str());
+	c=system(("cat out"+to_string(kmerList.size()-1)+".fa >> noduplicate.fa").c_str());
+	c=system((prefixCommand+"dsk -file noduplicate.fa -kmer-size 63 -abundance-min 1 -out out_dsk >>log 2>>log").c_str());
+	c=system(("echo noduplicate.fa > bankBready"));
+	c=system((prefixCommand+"BREADY -graph out_dsk -bank bankBready -query bankBready -out maximalSuperReads.fa -kmer_threshold 1 -fingerprint_size 60 -core 0 -gamma 10 >>log 2>>log").c_str());
 	cout<<"SuperReads Cleaning ended"<<endl;
 
 	cout<<"SuperReads Compaction"<<endl;
 	c=system((prefixCommand+"kMILL maximalSuperReads.fa >>log 2>>log").c_str());
+	c=system(("mv out_maximalSuperReads.fa.fa contigs.fa >>log 2>>log").c_str());
 	cout<<"SuperReads Cleaning ended"<<endl;
 
 	cout<<"The end"<<endl;
---
+++ install2.sh
@@ -0,0 +1,131 @@
+#!/bin/bash
+
+
+
+#THIS INSTALATION NEED GCC, GIT, MAKE, CMAKE
+
+
+
+function help {
+echo "BWISE instalation script"
+echo "This instalation need GCC>=4.9, GIT, MAKE, CMAKE3"
+echo "-f absolute path of folder to put the binaries (default ~/BWISEBin)"
+echo "-t to use multiple thread for compilation (default 8)"
+}
+
+
+
+threadNumber=8
+folder=""
+
+
+while getopts "hf:t:" opt; do
+case $opt in
+h)
+help
+exit
+;;
+f)
+echo "use folder: $OPTARG" >&2
+folder=$OPTARG
+;;
+s)
+echo "use  $OPTARG threads" >&2
+threadNumber=$OPTARG
+;;
+\?)
+echo "Invalid option: -$OPTARG" >&2
+exit 1
+;;
+:)
+echo "Option -$OPTARG requires an argument." >&2
+exit 1
+;;
+esac
+done
+
+if [ -z "$folder"  ]; then
+	help
+	exit 0
+	fi
+
+
+
+mkdir $folder;
+
+
+
+echo PHASE ZERO LAUNCHER: BWISE;
+make LOL=-Dfolder=$folder -j $theadNumber >>logCompile 2>>logCompile;
+cp bwise $folder;
+
+
+
+echo PHASE ONE, READ CORRECTION: BLOOCOO;
+git clone --recursive https://github.com/GATB/bloocoo.git >>logCompile 2>>logCompile;
+cd bloocoo;
+mkdir build; cd build;
+#TODO multiple bin
+cmake DKSIZE_LIST="64" .. >>logCompile 2>>logCompile;
+make -j $threadNumber >>logCompile 2>>logCompile;
+cp bin/Bloocoo $folder;
+cd ../..;
+
+
+
+
+echo PHASE TWO, GRAPH CONSTRUCTION: BCALM;
+git clone --recursive https://github.com/GATB/bcalm >>logCompile 2>>logCompile;
+cd bcalm;
+mkdir build; cd build;
+cmake .. -DKSIZE_LIST="32 64 96 128 160 192 224 256" >>logCompile 2>>logCompile;
+make -j $threadNumber >>logCompile 2>>logCompile;
+cp bcalm $folder;
+cd ../..;
+
+
+
+
+echo PHASE THREE, READ MAPPING ON THE DBG: BGREAT;
+git clone https://github.com/Malfoy/BGREAT2 >>logCompile 2>>logCompile;
+cd BGREAT2;
+make -j $threadNumber >>logCompile 2>>logCompile;
+cp bgreat $folder;
+cd ..;
+
+
+
+
+echo PHASE FOUR, SUPERREADS CLEANING: BREADY;
+git clone --recursive https://github.com/Malfoy/BREADY >>logCompile 2>>logCompile;
+cd BREADY;
+mkdir build; cd build;
+cmake .. >>logCompile 2>>logCompile;
+make -j $threadNumber >>logCompile 2>>logCompile;
+cp bin/BREADY $folder;
+cd ../..;
+
+
+
+
+echo and DSK;
+git clone --recursive https://github.com/GATB/dsk.git >>logCompile 2>>logCompile;
+cd dsk;
+mkdir build;  cd build;
+cmake -DKSIZE_LIST="64" .. >>logCompile 2>>logCompile;
+make -j $threadNumber >>logCompile 2>>logCompile;
+cp bin/dsk $folder;
+cd ../..;
+
+
+
+echo PHASE FIVE, MAXIMAL SUPERREADS COMPACTION: KMILL;
+git clone https://github.com/kamimrcht/kMILL >>logCompile 2>>logCompile;
+cd kMILL/src;
+make -j $threadNumber >>logCompile 2>>logCompile;
+cp kMILL $folder;
+cp pathsCleaner $folder;
+cd ../..;
+
+
+echo The end !;
