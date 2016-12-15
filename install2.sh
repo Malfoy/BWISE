bead77a8aa38af510177dcd04d109d26858c4b6b
Malfoy <antoine.limasset@gmail.com>
2016-12-15 16:17:55 +0100

    install bcalm debug

--- install2.sh
+++ install2.sh
@@ -106,7 +106,7 @@ eval  "
 git clone --recursive https://github.com/GATB/bcalm >>logCompile 2>>logCompile;
 cd bcalm;
 mkdir build; cd build;
-cmake .. -DKSIZE_LIST="32 64 96 128 160 192 224 256" >>logCompile 2>>logCompile;
+cmake -DKSIZE_LIST="32 64 96 128 160 192 224 256" ..  >>logCompile 2>>logCompile;
 make -j $threadNumber >>logCompile 2>>logCompile;
 cp bcalm $folder;
 cd ../..;
