3093aeed4d04dc759bdca3e91e9799d416f537b2
Pierre Peterlongo <pierre.peterlongo@inria.fr>
2017-03-09 17:26:01 +0100

    cosmetics

--- src/K2000/K2000.py
+++ src/K2000/K2000.py
@@ -51,13 +51,13 @@ def remove_strict_inclusions(SR):
     # pool = Pool(pool_size)
 
     for sr in SR.traverse():
-        if checked%100==0: sys.stderr.write("Removing inclusions, "+str(checked)+" checked. Size SR "+str(len(SR))+" %.2f"%(100*checked/n)+"%\r")
+        if checked%100==0: sys.stderr.write("      Removing inclusions, "+str(checked)+" checked. Size SR "+str(len(SR))+" %.2f"%(100*checked/n)+"%\r")
         checked+=1
         # pool.apply_async(remove_y_strictly_included_in_x(sr,SR,to_remove))
 
         remove_y_strictly_included_in_x(sr,SR)#,to_remove)
 
-    sys.stderr.write("Removing inclusions, "+str(checked)+" checked. Size SR "+str(len(SR))+" %.2f"%(100*checked/n)+"%\n")
+    sys.stderr.write("      Removing inclusions, "+str(checked)+" checked. Size SR "+str(len(SR))+" %.2f"%(100*checked/n)+"%\n")
     # SR_2=[]
   #   for i in range(len(to_remove)):
   #       if not to_remove[i]:
@@ -152,126 +152,52 @@ def fusion (SR,x):
     SR.sorted_add(kc.get_reverse_sr(new))
     return 1
     
-    
-    
-        
-    
-    
-def fusion_old (SR,x):
-    ''' Main function. For a given super read x, we find y that overlap x, such that : 
-     1/ there exists no other y with bigger overlap
-     2/ there exists no other x' having an overlap with y and that does not overlap perfectly with x (left colinearity)
-     3/ there exists no other y' having an overlap with x and that does not overlap perfectly with y (right colinearity)
-    Returns -1 if x cannot be compacted, 0 if it may be compacted latter, 1 if it was compacted
-    '''
-    # print ("fusion", x)
-    n=len(x)
-    # print("x is ", x)
-    x_=kc.get_reverse_sr(x)
-    for len_u in range(n-1,0,-1): # ***** CONDITION 1/ *****
-        # print ("len(u)", len_u)
-        u=x[-len_u:]
-        # print("u=",u)
-        Y=SR.get_lists_starting_with_given_prefix(u)
-        if x in Y: Y.remove(x) # possible if x is repeated 2,2,2,2 for instance.
-        # print ("Y=",Y)
-        if len(Y)==0: continue  # No y starting with u
-
-        if len(Y)>1: return 0   # More than one unique y starting with u, for instance y and y'. Knowing that y is not included in y' it means necessary that y and y' are not colinear and that x is thus non compactable. 
-       
-        y=Y[0]
-        
-
-        
-        
-       
-        # ***** CONDITION 2/ *****
-        # get all x' such that LCSP(x',y) in [1,len(u)-1]
-        # # get all x' ending with a prefix of u ==
-        # get all x' starting with a reverse complement of a prefix of u
-        X=[]
-        ending_positions=[]
-        for size_prefix in range(1,len_u+1):
-            prefix_u=u[:size_prefix]
-            others = SR.get_lists_starting_with_given_prefix(kc.get_reverse_sr(prefix_u))
-            if len(others) >0:
-                X+=others
-                ending_positions+=[len_u-size_prefix for zz in range(len(others))]
-        # print ("X=",X,"ending_positions = ",ending_positions)
-        if len(ending_positions)>0 and not colinear(x_,X,ending_positions): return 0# ending positions are starting positions of reversed sequences
-        
-       
-        # ***** CONDITION 3/ *****
-        # get all y' such that LCSP(x,y') in [1,len(u)-1]
-        # get all y' starting with a suffix of u
-        Y=[]
-        starting_positions=[]
-        for starting_suffix_position in range(0,len_u):
-            suffix_u=u[starting_suffix_position:]
-            others = SR.get_lists_starting_with_given_prefix(suffix_u)
-            if len(others) >0:
-                Y+=others
-                starting_positions+=[starting_suffix_position for zz in range(len(others))]
-        # print ("Y=",Y,"ending_positions = ",starting_positions)
-        if len(starting_positions)>0 and not colinear(y,Y,starting_positions): return 0
-            
-        
-        # print ("fusionning ",x,"with",y,", generating", x+y[len_u:])
-        # ***** FUSION *****
-        # print ("remove",x)
-        SR.remove(x)
-        # print ("remove",x_)
-        SR.remove(x_)
-        # print ("remove",y)
-        SR.remove(y)
-        SR.remove(kc.get_reverse_sr(y))
-        # print ("remove",kc.get_reverse_sr(y))
-        new=x+y[len_u:]
-        SR.sorted_add(new)
-        SR.sorted_add(kc.get_reverse_sr(new))
-        return 1
          
-
-             
-def main():
-    '''
-    Compaction of set of super reads coded as set of ids of unitigs
-    '''
-    # SR=[[1,3,4],[14],[4,6],[-6,-1,-2],[4,6,7,9],[9,10,11,12],[4,6],[-13, -12, -11]]
-
-    sys.stderr.write("Load super reads \n")
-    SR=kc.generate_SR(sys.argv[1])
-    # SR.unique()
-    sys.stderr.write("Add reverse complements "+ str(len(SR))+"\n")
-    kc.add_reverse_SR(SR)
-    SR.unique()
-    sys.stderr.write("Sorting"+ str(len(SR))+"\n")
-    SR.sort()
-    sys.stderr.write("Remove strict inclusions "+ str(len(SR))+"\n")
-    SR=remove_strict_inclusions(SR)
-    
-    # print("SR",SR)
-    # dont_try = sorted_list.sorted_list()
+def compaction(SR):
     checked=0
     compacted=0
     n = len(SR)
     for sr in SR.traverse():
         if checked%100==0: 
-            sys.stderr.write("Compacting, "+str(checked)+" checked. Size SR "+str(len(SR))+" %.2f"%(100*checked/n)+"%, "+str(compacted)+" nodes were compacted\r")
+            sys.stderr.write("      Compacting, "+str(checked)+" checked. Size SR "+str(len(SR))+" %.2f"%(100*checked/n)+"%, "+str(compacted)+" couple of nodes compacted\r")
         checked+=1
-        # if dont_try.contains(sr):
-            # continue
         witness = fusion(SR,sr)
         if witness == 1: # a fusion was done
             compacted+=1
-        # elif witness == -1: # no fusion done and sr no compactable
-            # dont_try.sorted_add(sr)
-            # print ("dont try", sr)
-        
-        # if witness == 0 : we do nothing, sr, may be compacted latter. 
-    sys.stderr.write("Compacting, "+str(checked)+" checked. Size SR "+str(len(SR))+" %.2f"%(100*checked/n)+"%, "+str(compacted)+" nodes were compacted\n")
+    sys.stderr.write("      Compacting, "+str(checked)+" checked. Size SR "+str(len(SR))+" %.2f"%(100*checked/n)+"%, "+str(compacted)+" couple of nodes compacted\n")
+    return SR
     
-    sys.stderr.write("Print results "+ str(len(SR))+"\n")
+             
+def main():
+    '''
+    Compaction of set of super reads coded as set of ids of unitigs
+    '''
+    sys.stderr.write("  Load super reads \r")
+    SR=kc.generate_SR(sys.argv[1])
+    sys.stderr.write("  Load super reads. Done - nb SR="+ str(len(SR))+"\n")
+
+    sys.stderr.write("  Add reverse complements \r")
+    kc.add_reverse_SR(SR)
+    sys.stderr.write("  Add reverse complements. Done - nb SR="+ str(len(SR))+"\n")
+    
+    sys.stderr.write("  Conserve only unique super reads \r")
+    SR.unique()
+    sys.stderr.write("  Conserve only unique super reads. Done - nb SR="+ str(len(SR))+"\n")
+    
+    sys.stderr.write("  Sorting \r")
+    SR.sort()
+    sys.stderr.write("  Sorting. Done - nb SR="+ str(len(SR))+"\n")
+    
+    sys.stderr.write("  Remove strict inclusions\r")
+    SR=remove_strict_inclusions(SR)
+    sys.stderr.write("  Remove strict inclusions. Done - nb SR="+ str(len(SR))+"\n")
+
+    sys.stderr.write("  Compaction of simple paths \r")
+    SR=compaction(SR)
+    sys.stderr.write("  Compaction of simple paths. Done - nb SR="+ str(len(SR))+"\n")
+    
+    
+    sys.stderr.write("  Print maximal super reads - nb MSR="+ str(len(SR))+"\n")
     kc.print_maximal_super_reads(SR)
 
 if __name__ == "__main__":
