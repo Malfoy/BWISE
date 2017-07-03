b3cb9c2e437432152bcfa0bff9b53dbfe8464165
Malfoy <antoine.limasset@gmail.com>
2017-07-03 22:11:57 +0200

    push to update triming parameters and rebuild

--- Bwise.py
+++ Bwise.py
@@ -238,9 +238,9 @@ def graphConstruction(BWISE_MAIN, BWISE_INSTDIR, OUT_DIR, fileBcalm, k_max, soli
 				print("\t\t #Graph cleaning... ", flush=True)
 				# BTRIM
 				if(solidity == 1):
-					cmd=BWISE_INSTDIR + "/btrim out.unitigs.fa "+kmerSize+" "+str(2*int(kmerSize)+1)+" "+coreUsed+" 8"
+					cmd=BWISE_INSTDIR + "/btrim out.unitigs.fa "+kmerSize+" "+str(2*int(kmerSize-1))+" "+coreUsed+" 8"
 				else:
-					cmd=BWISE_INSTDIR + "/btrim out.unitigs.fa "+kmerSize+" "+str(2*int(kmerSize)+1)+" "+coreUsed+" 8 "+str(unitigCoverage)
+					cmd=BWISE_INSTDIR + "/btrim out.unitigs.fa "+kmerSize+" "+str(2*int(kmerSize-1))+" "+coreUsed+" 8 "+str(unitigCoverage)
 				printCommand("\t\t\t"+cmd)
 				p = subprocessLauncher(cmd, logTipsToWrite, logTipsToWrite)
 				checkWrittenFiles(OUT_DIR + "/tipped_out.unitigs.fa")
